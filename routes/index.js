a3cd6b47d916a50efcdb0b590f4c9c279b09ab53
Aluno <Aluno@fatecdiadema.com>
2023-01-26 12:33:35 -0300

    usando mustache

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,11 @@ const { query } = require('express');
 const express = require('express')
 
 const router = express.Router();
-
-router.get('/posts/:slug', (req, res) => {
-    let slug = req.params.slug;
-    res.send('Slug do post: ' + slug);
-});
-
-router.get('/rota1', (req, res) => {
-    res.send("Rota 1")
-});
-
-router.get('/rota2', (req, res) => {
-    let  nome = req.query.nome
-    let  sobrenome = req.query.sobrenome
-    let  idade = req.query.idade
-
-    res.json({
-        dados: nome + ' ' + sobrenome + ' tem ' + idade + ' anos'
-    })
-
-    //res.send("Olá " + nome + ", você tem " + idade + " anos de idade");
+router.get('/', (req, res) => {
+    let obj = {
+        nome: req.query.nome
+    }
+    res.render('home', obj);
 });
 
 module.exports = router;
